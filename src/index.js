0e6d0e55cb4a908636647469e745ee3fc3fa4873
Mac <mac@Macs-MacBook-Pro.local>
2019-07-25 00:08:59 +0200

    Set up Redux to Make User Data Available on Global State.

--- src/index.js
+++ src/index.js
@@ -3,22 +3,49 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom'
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-
+import firebase from './firebase';
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-)
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension';
+import setUser from './actions';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+
+const store = createStore(() => {}, composeWithDevTools());
+
+class Root extends React.Component{
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                console.log(user);
+                this.props.setUser(user);
+                this.props.history.push('/');   
+            }
+        })
+    }
+render(){
+    return(
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+        )
+    }
+}
+
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+
+ReactDOM.render(
+    <Provider store={store}>
+    <Router>
+    <RootWithAuth />
+    </Router>
+    </Provider>
+, document.getElementById('root'));
 
 serviceWorker.unregister();
