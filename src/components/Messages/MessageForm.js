b6ff006777608a0fb9c7b4b340cf3628b476afd1
Abdelsalam-Megahed <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2019-08-14 11:52:59 +0300

    Emoji Picker to Add Emojis to Messages

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -2,6 +2,8 @@ import React from "react";
 import uuidv4 from "uuid/v4";
 import firebase from "../../firebase";
 import { Segment, Button, Input } from "semantic-ui-react";
+import {Picker, emojiIndex} from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 import FileModal from "./FileModal";
 import ProgressBar from "./ProgressBar";
@@ -18,7 +20,8 @@ class MessageForm extends React.Component {
     user: this.props.currentUser,
     loading: false,
     errors: [],
-    modal: false
+    modal: false,
+    emojiPicker: false
   };
 
   openModal = () => this.setState({ modal: true });
@@ -32,7 +35,6 @@ class MessageForm extends React.Component {
   handleKeyDown = () => {
     const { message, typingRef, channel, user } = this.state;
 
-     
     if (message) {
       typingRef
         .child(channel.id)
@@ -165,12 +167,47 @@ class MessageForm extends React.Component {
       });
   };
 
+  handleTogglePicker = () => {
+    this.setState({emojiPicker: !this.state.emojiPicker})
+  }
+
+  handleAddEmoji = (emoji) =>{
+    const oldMessage = this.state.message;
+    const newMessage = this.colonToUnicode(` ${oldMessage}${emoji.colons} `);
+    this.setState({message: newMessage, emojiPicker: false})
+  }
+
+  colonToUnicode = message => {
+    return message.replace(/:[A-Za-z0-9_+-]+:/g, x => {
+      x = x.replace(/:/g, "");
+      let emoji = emojiIndex.emojis[x];
+      if (typeof emoji !== "undefined") {
+        let unicode = emoji.native;
+        if (typeof unicode !== "undefined") {
+          return unicode;
+        }
+      }
+      x = ":" + x + ":";
+      return x;
+    });
+  };
+
+
   render() {
     // prettier-ignore
-    const { errors, message, loading, modal, uploadState, percentUploaded } = this.state;
+    const { errors, message, loading, modal, uploadState, percentUploaded, emojiPicker } = this.state;
 
     return (
       <Segment className="message__form">
+        {emojiPicker && (
+          <Picker
+            set="apple"
+            className="emojipicker"
+            title="Pick up your emoji"
+            emoji="point_up"
+            onSelect={this.handleAddEmoji}
+          />
+        )}
         <Input
           fluid
           name="message"
@@ -178,7 +215,12 @@ class MessageForm extends React.Component {
           onKeyDown={this.handleKeyDown}
           value={message}
           style={{ marginBottom: "0.7em" }}
-          label={<Button icon={"add"} />}
+          label=
+              {<Button 
+                icon={emojiPicker ? "close" : "add"} 
+                content={emojiPicker ? "close" : null}
+                onClick={this.handleTogglePicker} 
+              />}
           labelPosition="left"
           className={
             errors.some(error => error.message.includes("message"))
